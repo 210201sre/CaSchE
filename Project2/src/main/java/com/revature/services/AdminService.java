4c89cc3087dcbeaea578462d17731aad7097c47c
Ben Cady <bjcady14@gmail.com>
2021-03-22 23:48:30 -0500

    added more unit tests, some item service unit tests

--- Project2/src/main/java/com/revature/services/AdminService.java
+++ Project2/src/main/java/com/revature/services/AdminService.java
@@ -287,7 +287,7 @@ public class AdminService /* extends EmployeeService */ {
 		return ResponseEntity.ok().body("Transaction Item Deleted");
 	}
 
-	public List<Coupon> displayCoupons(Key k) {
+	public List<Coupon> displayCoupons(Key k) { //*********
 		MDC.put("Action", "Adm Display Coupons");
 		return coupDAO.findAll();
 	}
@@ -346,7 +346,7 @@ public class AdminService /* extends EmployeeService */ {
 		return ci;
 	}
 
-	private double calculateTotal(List<CartItem> cis) {
+	public double calculateTotal(List<CartItem> cis) { //************
 		double total = 0.0;
 		double coupon = 00.00;
 		double tax = 00.00;
--- Project2/src/test/java/com/revature/AdminServiceTests.java
+++ Project2/src/test/java/com/revature/AdminServiceTests.java
@@ -10,8 +10,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
+import com.revature.models.CartItem;
 import com.revature.models.Coupon;
+import com.revature.models.Item;
 import com.revature.models.Key;
 import com.revature.models.User;
 import com.revature.repositories.CouponDAO;
@@ -36,9 +40,9 @@ public class AdminServiceTests {
 	
 //	@Test
 //	void addUser() {
+//		Mockito.doReturn("User Created").when(userDAO).save(b);
 //		Mockito.doReturn(null).when(userDAO).findByUname("bcady");
-//		Assertions.assertEquals("User Created", adminService.addUser(k1, b));
-//		
+//		Assertions.assertEquals("User Created", adminService.addUser(k1, b));		
 //	}
 	
 	@Test
@@ -56,6 +60,26 @@ public class AdminServiceTests {
 		cpList.add(c1); cpList.add(c2);
 		Mockito.doReturn(cpList).when(coupDAO).findAll();
 		Assertions.assertEquals(cpList, adminService.displayCoupons(k1));
+	}
+	
+//	@Test
+//	void modCoupon() {
+//		Coupon c1 = new Coupon(0, "Food", "Food Discount", 3.2, 45);
+//		Mockito.doReturn("Coupon Updated").when(coupDAO).save(c1);
+//		Mockito.doReturn(true).when(coupDAO).existsById(c1.getCid());
+//		ResponseEntity<String> t = new ResponseEntity<String>("Coupon Updated", HttpStatus.ACCEPTED);
+//		Assertions.assertEquals(t, adminService.modCoupon(k1, c1));
+//	}
+	
+	@Test
+	void calculateTotal() {
+		Item i1 = new Item(456, "Cateloupe", "Fruit", 1, 3.20, 1, null, 4.00, 6);
+		Item i2 = new Item(2478, "Cheese", "", 1, 1.20, 1, null, 2.00, 6);
+		CartItem ci1 = new CartItem(1,1,1, i1);
+		CartItem ci2 = new CartItem(2,1,2,i2);
+		List<CartItem> cis = new ArrayList<CartItem>(); cis.add(ci1); cis.add(ci2);
+		AdminService as = new AdminService();
+		Assertions.assertEquals(4.40, as.calculateTotal(cis));
 	}
 	
 }
