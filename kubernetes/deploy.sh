be3d82249e765fb3a776c60017b947b02c341cd8
joneschmitz <joneschmitz@gmail.com>
2021-03-24 15:01:50 -0500

    Some logging change

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Generating Logs"
 echo ""
-echo "Secrets" >> logs
+echo "Secrets:" >> logs
 kubectl describe -n casche secret casche-credentials >> logs
 echo "Pods:" >> logs
 kubectl describe -n casche pods >> logs
@@ -9,11 +9,11 @@ echo "Deployment:" >> logs
 kubectl describe -n casche deployment project-two >> logs
 echo "Service:" >> logs
 kubectl describe -n casche service project-two >> logs
-echo "Loki-External" >> logs
+echo "Loki-External:" >> logs
 kubectl describe -n casche service loki >> logs
-echo "Ingress" >> logs
+echo "Ingress:" >> logs
 kubectl describe -n casche ing project-two >> logs
-echo "ServiceMonitor"
+echo "ServiceMonitor:" >> logs
 kubectl describe -n casche ServiceMonitor project-two >> logs
 echo "=======================================================" >> logs
 echo ""
@@ -37,3 +37,4 @@ echo ""
 echo "Displaying Unmodified Data"
 kubectl get -n casche ing
 kubectl get -n casche secrets
+kubectl get -n casche configmap
