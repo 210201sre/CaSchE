88f8fcc0b30dee3407a4bbe80064253750c321fd
joneschmitz <joneschmitz@gmail.com>
2021-03-25 10:11:34 -0500

    added logging the error outputs to file 'err'

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -21,12 +21,13 @@ echo "Reapplying some manifests"
 kubectl config use-context sre;
 # Must have proper system variables to connect to database or must have identical database in RDS to run the below command.
 # kubectl create -n casche secret generic casche-credentials --from-literal=url=$DB_URL --from-literal=username=$DB_USERNAME --from-literal=password=$DB_PASSWORD
-kubectl create -n casche configmap fluent-conf --from-file fluent.conf
-kubectl apply -n casche -f loki-external.yml
-kubectl apply -n casche -f service-p2.yml
-# kubectl apply -n casche -f ingress-p2.yml
-kubectl apply -n casche -f deployment-p2.yml
-kubectl apply -n casche -f svcmonitor.yml
+echo "=======================================================" >> err
+kubectl create -n casche configmap fluent-conf --from-file fluent.conf >> err
+kubectl apply -n casche -f loki-external.yml >> err
+kubectl apply -n casche -f service-p2.yml >> err
+# kubectl apply -n casche -f ingress-p2.yml >> err
+kubectl apply -n casche -f deployment-p2.yml >> err
+kubectl apply -n casche -f svcmonitor.yml >> err
 echo ""
 echo "Restarting Deployment"
 kubectl -n casche rollout restart deployment
