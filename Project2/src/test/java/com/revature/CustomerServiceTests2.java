d9cb924e98817ab44a12319595f15d3e81dd0a50
Ben Cady <bjcady14@gmail.com>
2021-03-25 23:07:39 -0500

    Added tests

--- Project2/src/test/java/com/revature/CustomerServiceTests2.java
+++ Project2/src/test/java/com/revature/CustomerServiceTests2.java
@@ -1,7 +1,6 @@
 package com.revature;
 
-import static org.mockito.Mockito.doNothing;
-
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
@@ -11,12 +10,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import com.revature.exceptions.InvalidException;
+import com.revature.models.CartItem;
 import com.revature.models.CartItemProto;
+import com.revature.models.Item;
 import com.revature.models.Key;
+import com.revature.models.Transaction;
 import com.revature.models.User;
 import com.revature.repositories.BackorderDAO;
 import com.revature.repositories.CartDAO;
@@ -86,4 +87,64 @@ public class CustomerServiceTests2 {
 		ResponseEntity<String> res = InvalidException.thrown(String.format("UPDATE: User %d mismatch %d", cip.getUid(), k.getUid()), new RuntimeException());
 		Assertions.assertEquals(res, cServ.delMyCartItem(cip, k));
 	}
+	
+	@Test
+	void emptyCart() {
+		Key k = new Key();
+		Mockito.when(cDAO.countByUid(k.getUid())).thenReturn((long) 0);
+		ResponseEntity<String> res = InvalidException.thrown(String.format("DELETE: User %d cart is empty.", k.getUid()), new RuntimeException());
+		Assertions.assertEquals(res, cServ.emptyCart(k));
+	}
+	
+	@Test
+	void displayTransactionItems() {
+		Key k = new Key();
+		Transaction t = new Transaction(); t.setTid(30); k.setUid(5); t.setUid(5);
+		List<CartItem> lci = null;
+		ResponseEntity<List<CartItem>> res = ResponseEntity.status(400).body(lci);
+		Assertions.assertEquals(res, cServ.displayTransactionItems(t, k));
+	}
+	
+	@Test
+	void setNewQuantities() {
+		Item i = new Item();
+		Mockito.when(iDAO.existsById(i.getIid())).thenReturn(false);
+		Assertions.assertEquals(false, cServ.setNewQuantities(i, (long) 7000));
+	}
+	
+	@Test
+	void buildCartItem() {
+		CartItemProto cip1 = new CartItemProto(21, 22, 3, 24);
+		Mockito.when(iDAO.findById(cip1.getIid())).thenReturn(Optional.empty());
+		CartItem p = new CartItem();
+		Assertions.assertEquals(p, cServ.buildCartItem(cip1));
+		
+	}
+	
+	@Test
+	void addUsr2() {
+		User b = new User(); Optional<User> bo = Optional.ofNullable(b);
+		Mockito.when(userDAO.findByUname(b.getUname())).thenReturn(bo);
+		ResponseEntity<String> res = InvalidException.thrown(String.format("INSERT: Username %s already exists.", b.getUname()), new RuntimeException());
+		Assertions.assertEquals(res, cServ.addUsr(b));
+	}
+	
+	@Test
+	void addUsr3() {
+		User b = new User(); 
+		Mockito.when(userDAO.findByUname(b.getUname())).thenReturn(Optional.empty());
+		b.setUid((long) 6);
+		ResponseEntity<String> res = InvalidException.thrown(String.format("INSERT: Invalid ID(s) (%d:%d) passed during insertion.", b.getUid(), b.getSid()), new RuntimeException());
+		Assertions.assertEquals(res, cServ.addUsr(b));
+	}
+	
+	@Test
+	void addUsr4() {
+		User b = new User(); 
+		Mockito.when(userDAO.findByUname(b.getUname())).thenReturn(Optional.empty());
+		b.setUid(null); b.setSid(null); b.setAccesslevel("Admin");
+		ResponseEntity<String> res = InvalidException.thrown(String.format("INSERT: Invalid access. User: %s",b.toString()), new RuntimeException());
+		Assertions.assertEquals(res, cServ.addUsr(b));
+	}
+	
 }
