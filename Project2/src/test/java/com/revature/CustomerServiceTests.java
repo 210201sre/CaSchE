fd52663f7e59ea721429b7966529c80a4b5204ab
Ben Cady <bjcady14@gmail.com>
2021-03-26 05:50:12 -0500

    added test

--- Project2/src/test/java/com/revature/CustomerServiceTests.java
+++ Project2/src/test/java/com/revature/CustomerServiceTests.java
@@ -362,6 +362,15 @@ public class CustomerServiceTests {
 	}
 	
 	@Test
+	void addUsr5() {
+		User b = new User(); 
+		Mockito.when(userDAO.findByUname(b.getUname())).thenReturn(Optional.empty());
+		b.setSid((long) 6);
+		ResponseEntity<String> res = InvalidException.thrown(String.format("INSERT: Invalid ID(s) (%d:%d) passed during insertion.", b.getUid(), b.getSid()), new RuntimeException());
+		Assertions.assertEquals(res, cServ.addUsr(b));
+	}
+	
+	@Test
 	void addUsr4() {
 		User b = new User(); 
 		Mockito.when(userDAO.findByUname(b.getUname())).thenReturn(Optional.empty());
